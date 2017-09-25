d1824edf73b055ef4eb2e37d721b02a9670e23b0
Alex Lerikos <alerikos@gmail.com>
2017-09-25 13:36:08 -0700

    Added ios-10-3-arm64 cmake config

--- ios-10-3-arm64.cmake
+++ ios-10-3-arm64.cmake
@@ -1,17 +1,17 @@
 # Copyright (c) 2016, Ruslan Baratov
 # All rights reserved.
 
-if(DEFINED POLLY_IOS_11_0_CMAKE_)
+if(DEFINED POLLY_IOS_10_3_CMAKE_)
   return()
 else()
-  set(POLLY_IOS_11_0_CMAKE_ 1)
+  set(POLLY_IOS_10_3_CMAKE_ 1)
 endif()
 
 include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_clear_environment_variables.cmake")
 
 include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_init.cmake")
 
-set(IOS_SDK_VERSION 11.0)
+set(IOS_SDK_VERSION 10.3)
 set(POLLY_XCODE_COMPILER "clang")
 polly_init(
     "iOS ${IOS_SDK_VERSION} Universal (iphoneos) / \
@@ -30,6 +30,7 @@ set(CMAKE_MACOSX_BUNDLE YES)
 set(CMAKE_XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY "iPhone Developer")
 
 set(IPHONEOS_ARCHS arm64)
+set(IPHONESIMULATOR_ARCHS i386;x86_64)
 
 include("${CMAKE_CURRENT_LIST_DIR}/compiler/xcode.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/os/iphone.cmake")
